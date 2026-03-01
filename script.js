cd22f22528f0673fd058d0ddd21ce3dfcc1d31df
Vaibhav Sharma <92776886+Vaibhav992@users.noreply.github.com>
2026-03-01 22:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateTimer() {
+    const now = new Date();
+
+    // Format date & time like sample
+    const formatted = now.toLocaleString();
+
+    document.getElementById("timer").innerText = formatted;
+}
+
+// Run immediately
+updateTimer();
+
+// Update every second
+setInterval(updateTimer, 1000);
